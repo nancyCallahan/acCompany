7cf35fabf58b8f3738735de35b7c07726833c524
Unknown <cristinaroige75@gmail.com>
2017-02-08 16:24:41 +0100

    Modificació primera pagina
    He fet hola alba

--- var/cache/dev/appDevDebugProjectContainer.php
+++ var/cache/dev/appDevDebugProjectContainer.php
@@ -342,7 +342,7 @@ class appDevDebugProjectContainer extends Container
      */
     protected function getCache_AppService()
     {
-        $this->services['cache.app'] = $instance = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('IyPREfod4-', 0, (__DIR__.'/pools'));
+        $this->services['cache.app'] = $instance = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('+jiWYc2wdo', 0, (__DIR__.'/pools'));
 
         if ($this->has('monolog.logger.cache')) {
             $instance->setLogger($this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE));
@@ -380,7 +380,7 @@ class appDevDebugProjectContainer extends Container
      */
     protected function getCache_SystemService()
     {
-        return $this->services['cache.system'] = \Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('aHEKuEph89', 0, 'gW2lXeu8OrA2pC32Xdhzrr', (__DIR__.'/pools'), $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE));
+        return $this->services['cache.system'] = \Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('jumjx+-BMf', 0, 'RzbanaB+OPyZ3mNfwBdkMO', (__DIR__.'/pools'), $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE));
     }
 
     /**
@@ -411,7 +411,7 @@ class appDevDebugProjectContainer extends Container
 
         $c = new \Symfony\Bundle\FrameworkBundle\CacheWarmer\TemplateFinder($a, $b, ($this->targetDirs[3].'\\app/Resources'));
 
-        return $this->services['cache_warmer'] = new \Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerAggregate(array(0 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\TemplatePathsCacheWarmer($c, ${($_ = isset($this->services['templating.locator']) ? $this->services['templating.locator'] : $this->getTemplating_LocatorService()) && false ?: '_'}), 1 => $this->get('kernel.class_cache.cache_warmer'), 2 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\TranslationsCacheWarmer($this->get('translator')), 3 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\ValidatorCacheWarmer($this->get('validator.builder'), (__DIR__.'/validation.php'), \Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('cyhz8AaKGD', 0, 'gW2lXeu8OrA2pC32Xdhzrr', (__DIR__.'/pools'), $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE))), 4 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\RouterCacheWarmer($this->get('router')), 5 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\AnnotationsCacheWarmer(${($_ = isset($this->services['annotations.reader']) ? $this->services['annotations.reader'] : $this->getAnnotations_ReaderService()) && false ?: '_'}, (__DIR__.'/annotations.php'), ${($_ = isset($this->services['cache.annotations']) ? $this->services['cache.annotations'] : $this->getCache_AnnotationsService()) && false ?: '_'}), 6 => new \Symfony\Bundle\TwigBundle\CacheWarmer\TemplateCacheCacheWarmer($this, $c, array()), 7 => new \Symfony\Bundle\TwigBundle\CacheWarmer\TemplateCacheWarmer($this->get('twig'), new \Symfony\Bundle\TwigBundle\TemplateIterator($a, ($this->targetDirs[3].'\\app'), array())), 8 => new \Symfony\Bridge\Doctrine\CacheWarmer\ProxyCacheWarmer($this->get('doctrine'))));
+        return $this->services['cache_warmer'] = new \Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerAggregate(array(0 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\TemplatePathsCacheWarmer($c, ${($_ = isset($this->services['templating.locator']) ? $this->services['templating.locator'] : $this->getTemplating_LocatorService()) && false ?: '_'}), 1 => $this->get('kernel.class_cache.cache_warmer'), 2 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\TranslationsCacheWarmer($this->get('translator')), 3 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\ValidatorCacheWarmer($this->get('validator.builder'), (__DIR__.'/validation.php'), \Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('SFKIFGuxLf', 0, 'RzbanaB+OPyZ3mNfwBdkMO', (__DIR__.'/pools'), $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE))), 4 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\RouterCacheWarmer($this->get('router')), 5 => new \Symfony\Bundle\FrameworkBundle\CacheWarmer\AnnotationsCacheWarmer(${($_ = isset($this->services['annotations.reader']) ? $this->services['annotations.reader'] : $this->getAnnotations_ReaderService()) && false ?: '_'}, (__DIR__.'/annotations.php'), ${($_ = isset($this->services['cache.annotations']) ? $this->services['cache.annotations'] : $this->getCache_AnnotationsService()) && false ?: '_'}), 6 => new \Symfony\Bundle\TwigBundle\CacheWarmer\TemplateCacheCacheWarmer($this, $c, array()), 7 => new \Symfony\Bundle\TwigBundle\CacheWarmer\TemplateCacheWarmer($this->get('twig'), new \Symfony\Bundle\TwigBundle\TemplateIterator($a, ($this->targetDirs[3].'\\app'), array())), 8 => new \Symfony\Bridge\Doctrine\CacheWarmer\ProxyCacheWarmer($this->get('doctrine'))));
     }
 
     /**
@@ -771,7 +771,7 @@ class appDevDebugProjectContainer extends Container
     {
         $this->services['doctrine_cache.providers.doctrine.orm.default_metadata_cache'] = $instance = new \Doctrine\Common\Cache\ArrayCache();
 
-        $instance->setNamespace('sf2orm_default_ec677a9fe343ed2cb8ce4e3159f226ff1cc03d1ebc7f6ed52c8032a80e0e4bd8');
+        $instance->setNamespace('sf2orm_default_3eba945367411f07ce067b4a381b4680fe2923851ff1269e65a996a0d09d8865');
 
         return $instance;
     }
@@ -788,7 +788,7 @@ class appDevDebugProjectContainer extends Container
     {
         $this->services['doctrine_cache.providers.doctrine.orm.default_query_cache'] = $instance = new \Doctrine\Common\Cache\ArrayCache();
 
-        $instance->setNamespace('sf2orm_default_ec677a9fe343ed2cb8ce4e3159f226ff1cc03d1ebc7f6ed52c8032a80e0e4bd8');
+        $instance->setNamespace('sf2orm_default_3eba945367411f07ce067b4a381b4680fe2923851ff1269e65a996a0d09d8865');
 
         return $instance;
     }
@@ -805,7 +805,7 @@ class appDevDebugProjectContainer extends Container
     {
         $this->services['doctrine_cache.providers.doctrine.orm.default_result_cache'] = $instance = new \Doctrine\Common\Cache\ArrayCache();
 
-        $instance->setNamespace('sf2orm_default_ec677a9fe343ed2cb8ce4e3159f226ff1cc03d1ebc7f6ed52c8032a80e0e4bd8');
+        $instance->setNamespace('sf2orm_default_3eba945367411f07ce067b4a381b4680fe2923851ff1269e65a996a0d09d8865');
 
         return $instance;
     }
@@ -2024,7 +2024,7 @@ class appDevDebugProjectContainer extends Container
      */
     protected function getPropertyAccessorService()
     {
-        return $this->services['property_accessor'] = new \Symfony\Component\PropertyAccess\PropertyAccessor(false, false, \Symfony\Component\PropertyAccess\PropertyAccessor::createCache('0lqPzeI7sT', NULL, 'gW2lXeu8OrA2pC32Xdhzrr', $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE)));
+        return $this->services['property_accessor'] = new \Symfony\Component\PropertyAccess\PropertyAccessor(false, false, \Symfony\Component\PropertyAccess\PropertyAccessor::createCache('+FXlE5vgFc', NULL, 'RzbanaB+OPyZ3mNfwBdkMO', $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE)));
     }
 
     /**
@@ -2220,7 +2220,7 @@ class appDevDebugProjectContainer extends Container
 
         $f = new \Symfony\Component\Security\Http\AccessMap();
 
-        return $this->services['security.firewall.map.context.main'] = new \Symfony\Bundle\SecurityBundle\Security\FirewallContext(array(0 => new \Symfony\Component\Security\Http\Firewall\ChannelListener($f, new \Symfony\Component\Security\Http\EntryPoint\RetryAuthenticationEntryPoint(80, 443), $a), 1 => new \Symfony\Component\Security\Http\Firewall\ContextListener($b, array(0 => new \Symfony\Component\Security\Core\User\InMemoryUserProvider()), 'main', $a, $this->get('debug.event_dispatcher', ContainerInterface::NULL_ON_INVALID_REFERENCE), $c), 2 => new \Symfony\Component\Security\Http\Firewall\AnonymousAuthenticationListener($b, '5899f60a142931.36153987', $a, $d), 3 => new \Symfony\Component\Security\Http\Firewall\AccessListener($b, ${($_ = isset($this->services['debug.security.access.decision_manager']) ? $this->services['debug.security.access.decision_manager'] : $this->getDebug_Security_Access_DecisionManagerService()) && false ?: '_'}, $f, $d)), new \Symfony\Component\Security\Http\Firewall\ExceptionListener($b, $c, new \Symfony\Component\Security\Http\HttpUtils($e, $e), 'main', NULL, NULL, NULL, $a, false), new \Symfony\Bundle\SecurityBundle\Security\FirewallConfig('main', 'security.user_checker', NULL, true, false, 'security.user.provider.concrete.in_memory', 'main', NULL, NULL, NULL, array(0 => 'anonymous')));
+        return $this->services['security.firewall.map.context.main'] = new \Symfony\Bundle\SecurityBundle\Security\FirewallContext(array(0 => new \Symfony\Component\Security\Http\Firewall\ChannelListener($f, new \Symfony\Component\Security\Http\EntryPoint\RetryAuthenticationEntryPoint(80, 443), $a), 1 => new \Symfony\Component\Security\Http\Firewall\ContextListener($b, array(0 => new \Symfony\Component\Security\Core\User\InMemoryUserProvider()), 'main', $a, $this->get('debug.event_dispatcher', ContainerInterface::NULL_ON_INVALID_REFERENCE), $c), 2 => new \Symfony\Component\Security\Http\Firewall\AnonymousAuthenticationListener($b, '589b37bfe7e599.03727352', $a, $d), 3 => new \Symfony\Component\Security\Http\Firewall\AccessListener($b, ${($_ = isset($this->services['debug.security.access.decision_manager']) ? $this->services['debug.security.access.decision_manager'] : $this->getDebug_Security_Access_DecisionManagerService()) && false ?: '_'}, $f, $d)), new \Symfony\Component\Security\Http\Firewall\ExceptionListener($b, $c, new \Symfony\Component\Security\Http\HttpUtils($e, $e), 'main', NULL, NULL, NULL, $a, false), new \Symfony\Bundle\SecurityBundle\Security\FirewallConfig('main', 'security.user_checker', NULL, true, false, 'security.user.provider.concrete.in_memory', 'main', NULL, NULL, NULL, array(0 => 'anonymous')));
     }
 
     /**
@@ -3242,6 +3242,7 @@ class appDevDebugProjectContainer extends Container
         $instance->addPath(($this->targetDirs[3].'\\vendor\\symfony\\symfony\\src\\Symfony\\Bundle\\TwigBundle/Resources/views'), 'Twig');
         $instance->addPath(($this->targetDirs[3].'\\vendor\\symfony\\swiftmailer-bundle/Resources/views'), 'Swiftmailer');
         $instance->addPath(($this->targetDirs[3].'\\vendor\\doctrine\\doctrine-bundle/Resources/views'), 'Doctrine');
+        $instance->addPath(($this->targetDirs[3].'\\src\\GestorDeProjecteBundle/Resources/views'), 'GestorDeProjecte');
         $instance->addPath(($this->targetDirs[3].'\\vendor\\symfony\\symfony\\src\\Symfony\\Bundle\\DebugBundle/Resources/views'), 'Debug');
         $instance->addPath(($this->targetDirs[3].'\\vendor\\symfony\\symfony\\src\\Symfony\\Bundle\\WebProfilerBundle/Resources/views'), 'WebProfiler');
         $instance->addPath(($this->targetDirs[3].'\\app/Resources/views'));
@@ -3491,7 +3492,7 @@ class appDevDebugProjectContainer extends Container
      */
     protected function getCache_AnnotationsService()
     {
-        return $this->services['cache.annotations'] = \Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('i3BfvJUmw7', 0, 'gW2lXeu8OrA2pC32Xdhzrr', (__DIR__.'/pools'), $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE));
+        return $this->services['cache.annotations'] = \Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('AXz4EMIn7Y', 0, 'RzbanaB+OPyZ3mNfwBdkMO', (__DIR__.'/pools'), $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE));
     }
 
     /**
@@ -3650,7 +3651,7 @@ class appDevDebugProjectContainer extends Container
      */
     protected function getSecurity_Authentication_ManagerService()
     {
-        $this->services['security.authentication.manager'] = $instance = new \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager(array(0 => new \Symfony\Component\Security\Core\Authentication\Provider\AnonymousAuthenticationProvider('5899f60a142931.36153987')), true);
+        $this->services['security.authentication.manager'] = $instance = new \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager(array(0 => new \Symfony\Component\Security\Core\Authentication\Provider\AnonymousAuthenticationProvider('589b37bfe7e599.03727352')), true);
 
         $instance->setEventDispatcher($this->get('debug.event_dispatcher'));
 
@@ -3925,6 +3926,11 @@ class appDevDebugProjectContainer extends Container
                     'path' => ($this->targetDirs[3].'\\src\\AppBundle'),
                     'namespace' => 'AppBundle',
                 ),
+                'GestorDeProjecteBundle' => array(
+                    'parent' => NULL,
+                    'path' => ($this->targetDirs[3].'\\src\\GestorDeProjecteBundle'),
+                    'namespace' => 'GestorDeProjecteBundle',
+                ),
                 'DebugBundle' => array(
                     'parent' => NULL,
                     'path' => ($this->targetDirs[3].'\\vendor\\symfony\\symfony\\src\\Symfony\\Bundle\\DebugBundle'),
@@ -3976,6 +3982,7 @@ class appDevDebugProjectContainer extends Container
                 'DoctrineBundle' => 'Doctrine\\Bundle\\DoctrineBundle\\DoctrineBundle',
                 'SensioFrameworkExtraBundle' => 'Sensio\\Bundle\\FrameworkExtraBundle\\SensioFrameworkExtraBundle',
                 'AppBundle' => 'AppBundle\\AppBundle',
+                'GestorDeProjecteBundle' => 'GestorDeProjecteBundle\\GestorDeProjecteBundle',
                 'DebugBundle' => 'Symfony\\Bundle\\DebugBundle\\DebugBundle',
                 'WebProfilerBundle' => 'Symfony\\Bundle\\WebProfilerBundle\\WebProfilerBundle',
                 'SensioDistributionBundle' => 'Sensio\\Bundle\\DistributionBundle\\SensioDistributionBundle',
---
+++ var/cache/dev/twig/c3/c30dc3ffb9a11dc0b3c300e4a928964fad9b66980605eb384c4dd4cc924a63bb.php
@@ -0,0 +1,177 @@
+<?php
+
+/* GestorDeProjecteBundle:Default:index.html.twig */
+class __TwigTemplate_c40fee91ae86f2408e350d4e2933faa60f590c774c8ee387d454657d060fd948 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'stylesheets' => array($this, 'block_stylesheets'),
+            'body' => array($this, 'block_body'),
+            'javascripts' => array($this, 'block_javascripts'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_5c59028aa099f184ee3e8e63cad4615565437e7ab97510233a31f9a5bd80b2a3 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_5c59028aa099f184ee3e8e63cad4615565437e7ab97510233a31f9a5bd80b2a3->enter($__internal_5c59028aa099f184ee3e8e63cad4615565437e7ab97510233a31f9a5bd80b2a3_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "GestorDeProjecteBundle:Default:index.html.twig"));
+
+        $__internal_507192e93b7a3c66772d15b738118f0b13e65e3ff36a99a3cc822afc0af1808a = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_507192e93b7a3c66772d15b738118f0b13e65e3ff36a99a3cc822afc0af1808a->enter($__internal_507192e93b7a3c66772d15b738118f0b13e65e3ff36a99a3cc822afc0af1808a_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "GestorDeProjecteBundle:Default:index.html.twig"));
+
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"es\">
+<head>
+    <meta charset=\"utf-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <title>";
+        // line 7
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+    ";
+        // line 8
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 9
+        echo "</head>
+
+<body>
+    ";
+        // line 12
+        $this->displayBlock('body', $context, $blocks);
+        // line 13
+        echo "
+    ";
+        // line 14
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 15
+        echo "</body>
+
+</html>
+
+";
+        
+        $__internal_5c59028aa099f184ee3e8e63cad4615565437e7ab97510233a31f9a5bd80b2a3->leave($__internal_5c59028aa099f184ee3e8e63cad4615565437e7ab97510233a31f9a5bd80b2a3_prof);
+
+        
+        $__internal_507192e93b7a3c66772d15b738118f0b13e65e3ff36a99a3cc822afc0af1808a->leave($__internal_507192e93b7a3c66772d15b738118f0b13e65e3ff36a99a3cc822afc0af1808a_prof);
+
+    }
+
+    // line 7
+    public function block_title($context, array $blocks = array())
+    {
+        $__internal_a8b5cb7061492741567fd608a6f183ba6c0508679cd07f26e0a4d1885acb9483 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_a8b5cb7061492741567fd608a6f183ba6c0508679cd07f26e0a4d1885acb9483->enter($__internal_a8b5cb7061492741567fd608a6f183ba6c0508679cd07f26e0a4d1885acb9483_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_3340fa8f9a7a7b70d00cdfe3c70f5f15f245523db4fe4ffd15bc06d27ca2935b = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_3340fa8f9a7a7b70d00cdfe3c70f5f15f245523db4fe4ffd15bc06d27ca2935b->enter($__internal_3340fa8f9a7a7b70d00cdfe3c70f5f15f245523db4fe4ffd15bc06d27ca2935b_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+
+        
+        $__internal_3340fa8f9a7a7b70d00cdfe3c70f5f15f245523db4fe4ffd15bc06d27ca2935b->leave($__internal_3340fa8f9a7a7b70d00cdfe3c70f5f15f245523db4fe4ffd15bc06d27ca2935b_prof);
+
+        
+        $__internal_a8b5cb7061492741567fd608a6f183ba6c0508679cd07f26e0a4d1885acb9483->leave($__internal_a8b5cb7061492741567fd608a6f183ba6c0508679cd07f26e0a4d1885acb9483_prof);
+
+    }
+
+    // line 8
+    public function block_stylesheets($context, array $blocks = array())
+    {
+        $__internal_e1c59967f632ed9508e298f4aaf94c9067fbf1dc10fdb992918d69e3c2e9c641 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_e1c59967f632ed9508e298f4aaf94c9067fbf1dc10fdb992918d69e3c2e9c641->enter($__internal_e1c59967f632ed9508e298f4aaf94c9067fbf1dc10fdb992918d69e3c2e9c641_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_02f694ca362fab431a3e953ebdc6df0ec50ed197c404484db2050b5b4d893989 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_02f694ca362fab431a3e953ebdc6df0ec50ed197c404484db2050b5b4d893989->enter($__internal_02f694ca362fab431a3e953ebdc6df0ec50ed197c404484db2050b5b4d893989_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        
+        $__internal_02f694ca362fab431a3e953ebdc6df0ec50ed197c404484db2050b5b4d893989->leave($__internal_02f694ca362fab431a3e953ebdc6df0ec50ed197c404484db2050b5b4d893989_prof);
+
+        
+        $__internal_e1c59967f632ed9508e298f4aaf94c9067fbf1dc10fdb992918d69e3c2e9c641->leave($__internal_e1c59967f632ed9508e298f4aaf94c9067fbf1dc10fdb992918d69e3c2e9c641_prof);
+
+    }
+
+    // line 12
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_ba95173b07092e0589e2643e975c95bcb1caab9935a0dc95beaf56fa53011013 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_ba95173b07092e0589e2643e975c95bcb1caab9935a0dc95beaf56fa53011013->enter($__internal_ba95173b07092e0589e2643e975c95bcb1caab9935a0dc95beaf56fa53011013_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_273d9250209341fd2105f16b8d6ecc2990f0bdd8e35a3ec5693961adb4847e89 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_273d9250209341fd2105f16b8d6ecc2990f0bdd8e35a3ec5693961adb4847e89->enter($__internal_273d9250209341fd2105f16b8d6ecc2990f0bdd8e35a3ec5693961adb4847e89_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        echo "Hello Alba";
+        
+        $__internal_273d9250209341fd2105f16b8d6ecc2990f0bdd8e35a3ec5693961adb4847e89->leave($__internal_273d9250209341fd2105f16b8d6ecc2990f0bdd8e35a3ec5693961adb4847e89_prof);
+
+        
+        $__internal_ba95173b07092e0589e2643e975c95bcb1caab9935a0dc95beaf56fa53011013->leave($__internal_ba95173b07092e0589e2643e975c95bcb1caab9935a0dc95beaf56fa53011013_prof);
+
+    }
+
+    // line 14
+    public function block_javascripts($context, array $blocks = array())
+    {
+        $__internal_2fb2ea1b1113510ace8b063617683d99f7c48773cd4b04624ccd8616060bc9df = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_2fb2ea1b1113510ace8b063617683d99f7c48773cd4b04624ccd8616060bc9df->enter($__internal_2fb2ea1b1113510ace8b063617683d99f7c48773cd4b04624ccd8616060bc9df_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+
+        $__internal_0c6b0fcbeb428170abba3a6591f36c596fa6232c861edb9dc2d399181a8d762b = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_0c6b0fcbeb428170abba3a6591f36c596fa6232c861edb9dc2d399181a8d762b->enter($__internal_0c6b0fcbeb428170abba3a6591f36c596fa6232c861edb9dc2d399181a8d762b_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+
+        
+        $__internal_0c6b0fcbeb428170abba3a6591f36c596fa6232c861edb9dc2d399181a8d762b->leave($__internal_0c6b0fcbeb428170abba3a6591f36c596fa6232c861edb9dc2d399181a8d762b_prof);
+
+        
+        $__internal_2fb2ea1b1113510ace8b063617683d99f7c48773cd4b04624ccd8616060bc9df->leave($__internal_2fb2ea1b1113510ace8b063617683d99f7c48773cd4b04624ccd8616060bc9df_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "GestorDeProjecteBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  121 => 14,  103 => 12,  86 => 8,  69 => 7,  55 => 15,  53 => 14,  50 => 13,  48 => 12,  43 => 9,  41 => 8,  37 => 7,  29 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<!DOCTYPE html>
+<html lang=\"es\">
+<head>
+    <meta charset=\"utf-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <title>{% block title %}{% endblock %}</title>
+    {% block stylesheets %}{% endblock %}
+</head>
+
+<body>
+    {% block body %}Hello Alba{% endblock %}
+
+    {% block javascripts %}{% endblock %}
+</body>
+
+</html>
+
+", "GestorDeProjecteBundle:Default:index.html.twig", "C:\\xampp\\htdocs\\acCompany\\src\\GestorDeProjecteBundle/Resources/views/Default/index.html.twig");
+    }
+}
